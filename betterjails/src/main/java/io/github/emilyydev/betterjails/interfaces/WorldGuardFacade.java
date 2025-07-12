9e77f30e1f2a4aea8739ce32dac70562859b2283
Emily <emilia.lopezf.1999@gmail.com>
2025-07-12 18:59:32 -0300

    Merge v1.6.1 changes onto v1 branch (#49)

--- betterjails/src/main/java/io/github/emilyydev/betterjails/interfaces/WorldGuardFacade.java
+++ betterjails/src/main/java/io/github/emilyydev/betterjails/interfaces/WorldGuardFacade.java
@@ -27,13 +27,11 @@ package io.github.emilyydev.betterjails.interfaces;
 import org.bukkit.entity.Player;
 
 import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
 
-import static java.lang.invoke.MethodHandles.Lookup;
 import static java.lang.invoke.MethodHandles.dropArguments;
-import static java.lang.invoke.MethodHandles.exactInvoker;
 import static java.lang.invoke.MethodHandles.filterArguments;
-import static java.lang.invoke.MethodHandles.filterReturnValue;
-import static java.lang.invoke.MethodHandles.lookup;
+import static java.lang.invoke.MethodHandles.foldArguments;
 import static java.lang.invoke.MethodType.methodType;
 
 public final class WorldGuardFacade {
@@ -42,9 +40,9 @@ public final class WorldGuardFacade {
 
   static {
     try {
-      final Lookup lookup = lookup();
-      MethodHandle resetState = lookup.findStatic(WorldGuardFacade.class, "empty", methodType(void.class));
-      resetState = dropArguments(resetState, 0, Player.class);
+      final MethodHandles.Lookup lookup = MethodHandles.lookup();
+      MethodHandle resetState = lookup.findStatic(WorldGuardFacade.class, "empty", methodType(void.class)); // ()void
+      resetState = dropArguments(resetState, 0, Player.class);                                              // (Player)void
       try {
         final Class<?> SessionManager = Class.forName("com.sk89q.worldguard.session.SessionManager");
         if (SessionManager.isInterface()) { // 7.x
@@ -63,17 +61,13 @@ public final class WorldGuardFacade {
           final MethodHandle WorldGuardPlatform_getSessionManager = lookup.findVirtual(WorldGuardPlatform, "getSessionManager", methodType(SessionManager));
           final MethodHandle SessionManager_resetState = lookup.findVirtual(SessionManager, "resetState", methodType(void.class, LocalPlayer));
 
-          MethodHandle wrapPlayer = WorldGuardPlugin_wrapPlayer;                                    // (WorldGuardPlugin,Player)LocalPlayer
-          wrapPlayer = filterArguments(wrapPlayer, 0, exactInvoker(methodType(WorldGuardPlugin)));  // (MethodHandle,Player)LocalPlayer
-          wrapPlayer = wrapPlayer.bindTo(WorldGuardPlugin_inst);                                    // (Player)LocalPlayer
+          MethodHandle wrapPlayer = WorldGuardPlugin_wrapPlayer;          // (WorldGuardPlugin,Player)LocalPlayer
+          wrapPlayer = foldArguments(wrapPlayer, WorldGuardPlugin_inst);  // (Player)LocalPlayer
 
-          MethodHandle getSessionManager = WorldGuard_getInstance;                                        // ()WorldGuard
-          getSessionManager = filterReturnValue(getSessionManager, WorldGuard_getPlatform);               // ()WorldGuardPlatform
-          getSessionManager = filterReturnValue(getSessionManager, WorldGuardPlatform_getSessionManager); // ()SessionManager
-
-          resetState = SessionManager_resetState;                                                             // (SessionManager,LocalPlayer)void
-          resetState = filterArguments(resetState, 0, exactInvoker(methodType(SessionManager)), wrapPlayer);  // (MethodHandle,Player)void
-          resetState = resetState.bindTo(getSessionManager);                                                  // (Player)void
+          resetState = SessionManager_resetState;                                                         // (SessionManager,LocalPlayer)void
+          resetState = filterArguments(resetState, 0, WorldGuardPlatform_getSessionManager, wrapPlayer);  // (WorldGuardPlatform,Player)void
+          resetState = filterArguments(resetState, 0, WorldGuard_getPlatform);                            // (WorldGuard,Player)void
+          resetState = foldArguments(resetState, WorldGuard_getInstance);                                 // (Player)void
         } // TODO: <=1.12
       } catch (final ClassNotFoundException ignored) {
       }
